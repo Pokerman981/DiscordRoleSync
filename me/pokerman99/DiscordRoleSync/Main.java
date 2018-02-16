6df3c5b29d7d6848ca58fdb09dd288c5a665e5a8
Pokerman981 <tjg7105@edu.wrsd.ca>
2018-02-16 15:26:30 -0700

    api7 update

---
+++ me/pokerman99/DiscordRoleSync/Main.java
@@ -0,0 +1,103 @@
+package me.pokerman99.DiscordRoleSync;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.event.game.state.GameStoppingEvent;
+import org.spongepowered.api.plugin.Dependency;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+@Plugin(id = "discordrolesync", name = "DiscordRoleSync", version = "1.0", dependencies = {
+		@Dependency(id = "luckperms", optional = false)
+	})
+public class Main{
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	private Path defaultConfig;
+
+	@Inject
+	@DefaultConfig(sharedRoot = false)
+	public ConfigurationLoader<CommentedConfigurationNode> loader;
+
+	@Inject
+	@ConfigDir(sharedRoot = false)
+	private Path ConfigDir;
+	
+	@Inject
+	public PluginContainer plugin;
+	public PluginContainer getPlugin() {
+		return this.plugin;
+	}
+
+	public static CommentedConfigurationNode rootNode;
+
+	public static CommentedConfigurationNode config() {
+		return rootNode;
+	}
+
+	public void save() throws IOException {
+		loader.save(rootNode);
+	}
+
+	@Inject
+	private Logger logger;
+
+	public Logger getLogger() {
+		return logger;
+	}
+	
+    public static Main instance;
+    
+    public static Main getInstance() {
+        return instance;
+    }
+    
+
+	@Listener
+	public void onPreInit(GameStartingServerEvent event) {
+		String server = Sponge.getGame().getServer().getMotd().toString().replace("{", "")
+				.replace("Text", "").replace("}", "");
+		me.pokerman99.DiscordRoleSync.discord.Login(server);
+		//Sponge.getEventManager().registerListeners(this, new serverInfoCommand());
+		Sponge.getEventManager().registerListeners(this, new me.pokerman99.DiscordRoleSync.roleSync(this));
+		try {rootNode = loader.load();} catch (IOException e1) {e1.printStackTrace();}
+		config().getNode("config-version").setValue("1.1");
+		try {loader.save(config());} catch (IOException e) {e.printStackTrace();}
+		CommandSpec link = CommandSpec.builder().executor(new me.pokerman99.DiscordRoleSync.roleSync(this)).build();
+		Sponge.getCommandManager().register(this, link, Lists.newArrayList("link"));
+		if (config().getNode("pins").isVirtual()){
+		List<String> temp = new ArrayList<String>();
+		temp.add("UbyZZQB7pmaCAv9w");
+		config().getNode("pins").setValue(temp);
+		try {loader.save(config());} catch (IOException e) {e.printStackTrace();}
+		}
+		if (config().getNode("linked").isVirtual()){
+		List<String> temp = new ArrayList<String>();
+		temp.add("pokerman99");
+		config().getNode("linked").setValue(temp);
+		try {loader.save(config());} catch (IOException e) {e.printStackTrace();}
+		}
+	}
+	
+	@Listener
+	public void onShutDown(GameStoppingEvent event){
+		me.pokerman99.DiscordRoleSync.discord.Logout();
+	}
+	
+}
---
+++ me/pokerman99/DiscordRoleSync/Ref.java
@@ -0,0 +1,14 @@
+package me.pokerman99.DiscordRoleSync;
+
+public class Ref {
+	public static final String main_token = "MjgyMDU2Nzc3MDAzMTcxODQx.DNPhEA.WHlUS8UCCE4gguOhe17YA4EN4GQ";
+	public static final String dev_token = "MzM1MTM1Mzk2Nzk3MjE4ODI2.DSttIw.y8F_b37cvb0D4FPiTUzyzrRH7Ic";
+	public static final String pokedash_token = "Mzk5MzQzMTY1MTQ1ODc0NDQ0.DTLt9w.R4sWHvhBMR4ZH15-Mxnjijj9uAk";
+	public static final String pokeverse_token = "Mzk5MzQ0NjMzODYxMjQyOTEx.DTLulg.VH4Nkvz2ZXrCmSMJ4w8_T3vwl-E";
+	public static final String pokeclub_token = "Mzk5MzQ1MjQxNjk5Nzc4NTYw.DTLu3Q.GhrCECTAmzSjnuqPjrmO7n2SnT4";
+	public static final String pokelegends_token = "Mzk5MzQ1NDc0MTU2NDk0ODQ4.DTL5KQ.VoOBqfXu-jyoDnaGAtjp5936nqM";
+	public static final String client_id = "282056777003171841";
+	public static final String owner_id = "126422606072119296";
+	public static final String pixplus_id = "258797004757532672";
+}
+ 
