c04cfbdc733f67d16e02e98adf807d1dc93d58d6
TroyG <troy.gidney@lethbridgecollege.ca>
2019-01-14 21:39:25 -0700

    Added checkPin command

--- src/main/java/me/pokerman99/DiscordRoleSync/Main.java
+++ src/main/java/me/pokerman99/DiscordRoleSync/Main.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.config.DefaultConfig;
@@ -22,6 +25,8 @@ import com.google.inject.Inject;
 
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.spongepowered.api.service.user.UserStorageService;
+import org.spongepowered.api.text.Text;
 
 @Plugin(id = "discordrolesync", name = "DiscordRoleSync", version = "1.0", dependencies = {
 		@Dependency(id = "luckperms", optional = false)
@@ -93,6 +98,29 @@ public class Main{
 		config().getNode("linked").setValue(temp);
 		try {loader.save(config());} catch (IOException e) {e.printStackTrace();}
 		}
+
+		CommandSpec checkPin = CommandSpec.builder().permission("discord.checkpin").arguments(GenericArguments.string(Text.of("name"))).executor((src, args) -> {
+				if (!args.getOne(Text.of("name")).isPresent()) {
+					roleSync.sendMessage(src, "&cYou must supply a user name");
+					return CommandResult.empty();
+				}
+
+				String name =  args.getOne(Text.of("name")).get().toString();
+				Optional<UserStorageService> userStorage = Sponge.getServiceManager().provide(UserStorageService.class);
+
+				if (!userStorage.get().get(name).isPresent()) {
+				roleSync.sendMessage(src, "&cUnable to find this player. Are they real?");
+				return CommandResult.empty();
+			}
+				String uuid = userStorage.get().get(name).get().getIdentifier();
+				String pin = rootNode.getNode("records", uuid, "pin").getString();
+
+				roleSync.sendMessage(src, "&a" + name + "'s pin is " + pin);
+			return  CommandResult.success();
+		}).build();
+
+		Sponge.getCommandManager().register(getInstance(), checkPin, "checkping");
+
 	}
 	
 	@Listener
